65831eeca930f4534760877611392654659c5620
luanalopez20 <luanaslopez@outlook.com>
2025-05-26 14:58:42 -0300

    Evaluacion

---
+++ Examen.js
@@ -0,0 +1,34 @@
+/*EJERCICIO 1*/
+
+const Numero = [3, 7, 2, 9, 12, 5, 8];
+if (Numero > 5) {
+  Menor = 0;
+  Mayor = 0;
+  for (const i = 0; i < Numero.length; i++) {
+    if (Numero[i] > 5) {
+      Menor += Numero[i];
+    } else {
+      Mayor++;
+    }
+  }
+}
+return Numero;
+
+/*EJERCICIO 2*/
+const Estudiantes = [
+  { Nombre: "Ana", Nota: 8 },
+  { Nombre: "Juan", Nota: 5 },
+  { Nombre: "Lucía", Nota: 9 },
+  { Nombre: "Pedro", Nota: 4 },
+];
+if ((Estudiantes = Nombre)) {
+}
+return Nombre;
+
+if ((Estudiantes = (Nota) => 6)) {
+}
+return (print = "Aprobado");
+
+if ((Estudiantes = Nota < 6)) {
+}
+return (print = "Desaprobado");
